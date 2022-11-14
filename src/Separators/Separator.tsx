c38a02fbec030526f06ebfffe191f734130d894a
Mauricio Mateus <helvert.mateus@gmail.com>
2022-11-14 06:55:38 -0500

    CHORE Optimized tracked state deps in Separator.tsx

--- src/Separators/Separator.tsx
+++ src/Separators/Separator.tsx
@@ -1,4 +1,10 @@
-import React, { CSSProperties, ReactElement, useEffect, useState } from 'react'
+import React, {
+  CSSProperties,
+  ReactElement,
+  useCallback,
+  useEffect,
+  useState
+} from 'react'
 import styled from 'styled-components'
 import { Direction } from '../hooks/useRefsWithInitialSize'
 
@@ -45,10 +51,13 @@ export const Separator = (props: SeparatorProps) => {
   const [mousePositionAtLastMouseEvent, setMousePositionAtLastMouseEvent] =
     useState<number | null>(null)
 
-  const getMovedDistance = (event: SeparatorDragEvent) => {
-    const mousePosition = getPositionFromDragEvent(props.direction, event)
-    return mousePosition - (mousePositionAtLastMouseEvent || 0)
-  }
+  const getMovedDistance = useCallback(
+    (event: SeparatorDragEvent) => {
+      const mousePosition = getPositionFromDragEvent(props.direction, event)
+      return mousePosition - (mousePositionAtLastMouseEvent || 0)
+    },
+    [mousePositionAtLastMouseEvent, props.direction]
+  )
   /**
    * Start dragging
    * Record the initial mouse position to calculate distance later
@@ -62,24 +71,30 @@ export const Separator = (props: SeparatorProps) => {
    * End dragging
    * Clear initial mouse position, which will stop mouse move handling
    */
-  const mouseUpEventHandler = (event: SeparatorDragEvent) => {
-    if (mousePositionAtLastMouseEvent) {
-      setMousePositionAtLastMouseEvent(null)
-    }
-  }
+  const mouseUpEventHandler = useCallback(
+    (event: SeparatorDragEvent) => {
+      if (mousePositionAtLastMouseEvent) {
+        setMousePositionAtLastMouseEvent(null)
+      }
+    },
+    [mousePositionAtLastMouseEvent]
+  )
   /**
    * Calculate distance from the initial mouse position when start dragging
    * Send the distance to parent component
    */
-  const mouseMoveEventHandler = (event: SeparatorDragEvent) => {
-    if (
-      mousePositionAtLastMouseEvent !== null &&
-      mousePositionAtLastMouseEvent !== undefined
-    ) {
-      const movedDistance = getMovedDistance(event)
-      props.onDrag(movedDistance)
-    }
-  }
+  const mouseMoveEventHandler = useCallback(
+    (event: SeparatorDragEvent) => {
+      if (
+        mousePositionAtLastMouseEvent !== null &&
+        mousePositionAtLastMouseEvent !== undefined
+      ) {
+        const movedDistance = getMovedDistance(event)
+        props.onDrag(movedDistance)
+      }
+    },
+    [getMovedDistance, mousePositionAtLastMouseEvent, props]
+  )
   /**
    * Listen to mouse move and mouse up at global because the position of the separator
    * will not be updated at mouse position instantly
@@ -97,7 +112,11 @@ export const Separator = (props: SeparatorProps) => {
         mouseMoveEventHandler
       )
     }
-  }, [mousePositionAtLastMouseEvent])
+  }, [
+    mouseMoveEventHandler,
+    mousePositionAtLastMouseEvent,
+    mouseUpEventHandler
+  ])
 
   return (
     <SeparatorDiv
